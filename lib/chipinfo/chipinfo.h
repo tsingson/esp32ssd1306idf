e1e571775aa60cac3e3b9708c3a8944761e6eaef
qinshen <tsingson@gmail.com>
2026-06-26 09:19:31 +0800

    add chip info

---
+++ lib/chipinfo/chipinfo.h
@@ -0,0 +1,12 @@
+//
+// Created by tsingson on 2026/6/21.
+//
+
+#ifndef LED_BRIGHTNESS_CONTROL_CHIPINFO_H
+#define LED_BRIGHTNESS_CONTROL_CHIPINFO_H
+
+void print_chip_info();
+
+char *get_chip_info_string_heap(void);
+
+#endif // LED_BRIGHTNESS_CONTROL_CHIPINFO_H
